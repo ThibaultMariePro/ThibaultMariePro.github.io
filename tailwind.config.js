9f888117e93b3abbcc8de645cd0effd9de1a8b71
TIBO <thibault.marie.pro@gmail.com>
2024-03-20 22:48:16 +0100

    WIP WIP WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  darkMode: 'selector',
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          1: "hsl(var(--color-accent1) / <alpha-value>)",
+          2: "hsl(var(--color-accent2) / <alpha-value>)",
+        },
+        bg:"hsl(var(--color-bg) / <alpha-value>)",
+        content:"hsl(var(--color-content) / <alpha-value>)",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated'),
+  ],
 }
