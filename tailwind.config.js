eedecc124e9bf4808067291d39ee358c6b0a2d60
TIBO <thibault.marie.pro@gmail.com>
2024-03-23 00:16:48 +0100

    tweaked blur effect on navbar design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        hlorange: "hsl(var(--color-hlorange) / <alpha-value>)",
-        portalblue: "hsl(var(--color-portalblue) / <alpha-value>)",
-        bg:"hsl(var(--color-bg) / <alpha-value>)",
-        content:"hsl(var(--color-content) / <alpha-value>)",
-        light:"hsl(var(--color-light) / <alpha-value>)",
-        dark:"hsl(var(--color-dark) / <alpha-value>)",
+        hlorange: "hsl(var(--color-hlorange)/<alpha-value>)",
+        portalblue: "hsl(var(--color-portalblue)/<alpha-value>)",
+        bg:"hsl(var(--color-bg)/<alpha-value>)",
+        content:"hsl(var(--color-content)/<alpha-value>)",
+        light:"hsl(var(--color-light)/<alpha-value>)",
+        dark:"hsl(var(--color-dark)/<alpha-value>)",
       },
     },
   },
