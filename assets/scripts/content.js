747e5fe1015e1d2e8fd21e6fc9b2a63467ce9f80
TIBO <thibault.marie.pro@gmail.com>
2024-11-08 22:05:30 +0100

    build: restructures assets
    add refactored script
    add images

---
+++ assets/scripts/content.js
@@ -0,0 +1,86 @@
+export const content = {
+  fr: {
+    lang: "fr",
+    switchLangBtn: "fr",
+    hello: "Salut le monde!",
+    job: "Développeur Web Fullstack | Concepteur Développeur d'Applications",
+    whoami: "Je suis un webdev très original, accro au café.",
+    consolecatchphrase:
+      "Vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise tech. Moi, si je devais résumer ma folie des internets aujourd’hui avec vous, je dirais que c’est d’abord des rencontres. Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi. Et c’est assez curieux de se dire que les hasards, les rencontres forgent une carrière dans cette folle époque du digital (je déteste quand on dit digital là où il faudrait dire numérique)... Parce que quand on a le goût de la chose, quand on a le goût de la chose bien faite, le beau geste, parfois on ne trouve pas l’interlocuteur en face je dirais, le débugueur qui vous aide à avancer. Alors ça n’est pas mon cas, comme je disais là, puisque moi au contraire, j’ai pu ; et je dis merci à la folie des internets, je lui dis merci, je chante la folie des internets, je danse la folie des internets... je ne suis qu’amour ! Et finalement, quand des gens me disent « Mais comment fais-tu pour avoir cette passion propre à ton époque ? », je leur réponds très simplement que c’est ce goût de l’amour, ce goût donc qui m’a poussé aujourd’hui à entreprendre une ascension ambitieuse... mais demain qui sait ? Peut-être simplement à me mettre au service de la communauté, à faire le don, le don de soi.",
+    mailme: "✉️ envoyez moi un mail :)",
+    careerTitle: "Parcours",
+    careerP1:
+      "Je suis Thibault MARIE, concepteur et développeur d'applications, animé par une passion pour l'efficacité et l'optimisation des systèmes de travail, tant techniques qu'humains. Je considère souvent que c'est une opportunité manquée lorsque des projets sont freinés par l'inertie : des équipes qui communiquent trop peu, du code qui mériterait d'être refactorisé et documenté, ou des workflows rigides et mal adaptés aux besoins uniques d'une organisation. Pour moi, chaque défi est une occasion d'investir dans un système plus fluide.",
+    careerP2:
+      "Ma carrière a débuté en 2013 avec ma première expérience en trading de cryptomonnaies en tant que freelance. Au fil des années, j'ai monté un rig de minage, conçu des algorithmes de prise de décision, et affiné mes compétences en gestion des risques. Ces activités m'ont appris à prendre des décisions rapides et réfléchies, en jonglant constamment avec les compromis de coût-bénéfice, ce qui a aiguisé mes capacités d'analyse et ma résilience.",
+    careerP3:
+      "En 2020, j'ai eu la chance de rejoindre une équipe travaillant sur une application visant à sensibiliser aux problématiques de sexisme en milieu professionnel. Ce projet, exigeant à la fois humainement et techniquement, m'a permis de contribuer à la sécurité des données et de m'engager dans un sujet significatif. À l'époque, je travaillais avec SailsJS et MongoDB. J'ai pris conscience de l'impact positif que peut avoir un code bien structuré et documenté sur la longévité et la capacité d'évolution d'un projet. Travaillant en binôme, j'ai appris à apprécier la reconfiguration des workflows pour développer efficacement et harmoniser différents styles de travail.",
+    careerP4:
+      "Durant ces années, j'ai également travaillé à valider des diplômes pour formaliser mes compétences, passant d'un apprenant passionné à un professionnel. J'ai obtenu deux diplômes consécutifs et finalement atteint le statut de Concepteur-Développeur d'Applications. En 2023, j'ai contribué au développement d'une application dédiée à la Qualité de Vie au Travail au sein d'une nouvelle équipe. Ce projet m'a permis de perfectionner mes compétences en gestion de projet et de travailler avec des technologies telles que Docker, Makefile, bash et htmx.",
+    careerP5:
+      "La grande variété des tâches et l'opportunité de m'investir en R&D m'ont permis d'explorer des sujets passionnants, de la sécurité serveur aux modèles UX documentés par des recherches scientifiques, en passant par les standards RFC. J'ai également eu l'opportunité de présenter un projet de migration pour une base de données impliquant restructuration et typage fort. J'ai travaillé avec le framework GaboX MVC, qui a renforcé l'importance d'une architecture modulaire et, comme toujours, de la documentation interne. En collaboration avec un collègue, j'ai réécrit notre documentation d'environnement, essentielle pour le travail dans un contexte technique très spécifique.",
+    careerP6:
+      "À ce stade, je me suis profondément investi dans la réduction de l'inertie des systèmes.",
+    careerP7:
+      "En mai 2024, fort de ce parcours, j'ai lancé reverse814, ma propre entreprise de développement, où je mets en œuvre une vision affinée au fil des années. Je m'implique dans chaque aspect de mes projets, offrant des environnements conteneurisés, une documentation structurée, et intégrant des technologies comme Symfony, VueJS et les API d'IA.",
+    careerP8:
+      " Mon approche se concentre sur la construction de relations solides avec les clients, l'identification de leurs besoins uniques, et la fourniture de solutions durables, personnalisées et optimisées. Dans chaque projet, mon objectif est d'établir des fondations solides qui facilitent la maintenance et l'évolutivité, réduisent l'inertie et favorisent la fluidité des équipes. Je poursuis mon parcours de découverte technique avec passion, sans crainte d'investir du temps dans la conception, l'observation des systèmes, et la critique constructive.",
+    workTitle: "Mon approche sur votre projet",
+  },
+  en: {
+    lang: "en",
+    switchLangBtn: "en",
+    hello: "Hello World!",
+    job: "Fullstack Web Developer | Application Designer Developer",
+    whoami: "I'm a very original webdev, coffee addict.",
+    consolecatchphrase:
+      "You know, I don't believe there is good or bad tech. For me, if I had to summarize my internet madness today with you, I would say that it is first and foremost encounters. People who reached out to me, maybe at a time when I couldn't, when I was alone at home. And it's quite curious to say that chance encounters forge a career in this crazy digital age (I hate it when people say digital when they should say digital)... Because when you have a taste for something, when you have a taste for doing things well, the beautiful gesture, sometimes you don't find the person in front of you, I would say, the debugger who helps you move forward. So that's not my case, as I was saying, since on the contrary, I was able to; and I thank the madness of the internet, I thank it, I sing the madness of the internet, I dance the madness of the internet... I am only love! And finally, when people ask me, “But how do you have this passion that is unique to your time?” I simply tell them that it is this taste for love, this taste that pushed me today to undertake an ambitious ascent... but tomorrow who knows? Maybe just to serve the community, to give, to give of oneself.",
+    mailme: "✉️ send me an email :)",
+    careerTitle: "Career",
+    careerP1:
+      "I am Thibault MARIE, an application designer and developer, driven by a passion for efficiency and optimizing work systems, both technical and human. I often feel it’s a missed opportunity when projects are held back by inertia: teams that communicate too little, code that deserves to be refactored and documented, or workflows that are rigid and poorly tailored to an organization’s unique needs. To me, every challenge is an opportunity to invest in making the system more fluid.",
+    careerP2:
+      "My career began in 2013 with my first experience in cryptocurrency trading as a freelancer. Over the years, I built a mining rig, designed decision-making algorithms, and honed my risk management skills. These activities taught me how to make swift and thoughtful decisions, constantly balancing cost-benefit trade-offs, which sharpened my analytical abilities and resilience.",
+    careerP3:
+      "In 2020, I was fortunate to join a team working on an application aimed at raising awareness around workplace sexism. This project, demanding on both human and technical fronts, allowed me to contribute to data security and engage with a meaningful topic. At that time, I worked with SailsJS and MongoDB. I became aware of the positive impact well-structured and documented code can have on a project’s longevity and ease of evolution. Working in pairs, I learned to appreciate rethinking workflows to develop efficiently and harmonize different work styles.",
+    careerP4:
+      "During these years, I also focused on validating diplomas to formalize my skills, transitioning from a passionate learner to a professional. I earned two consecutive degrees and ultimately achieved the status of Application Designer-Developer. In 2023, I contributed to the development of an application dedicated to Quality of Life at Work within a new team. This project allowed me to deepen my project management skills and work with technologies like Docker, Makefile, bash, and htmx.",
+    careerP5:
+      "The wide variety of tasks and the opportunity to engage in R&D enabled me to delve into fascinating topics, from server security to UX patterns documented by scientific research, to RFC standards. I also had the opportunity to present a migration project for a database that involved restructuring and strong typing. I worked with the GaboX MVC framework, which reinforced the importance of modular architecture and, as always, internal documentation. Collaborating with a colleague, I rewrote our environment documentation, essential for work in a highly specific technical context.",
+    careerP6:
+      "At that point, I became deeply engaged with the topic of reducing system inertia.",
+    careerP7:
+      "In May 2024, with this background, I launched reverse814, my own development company, where I implement a vision refined over the years. I’ve immersed myself in every aspect of my projects, offering containerized environments, structured documentation, and integrating technologies like Symfony, VueJS, and AI APIs.",
+    careerP8:
+      " My approach centers on building strong client relationships, identifying unique needs, and providing durable, customized, and optimized solutions. In every project, my goal is to establish solid foundations that facilitate maintenance and scalability, reduce inertia, and foster team fluidity. I continue my journey of technical discovery with passion, unafraid to invest time in design, system observation, and constructive critique.",
+    workTitle: "My approach on your project",
+  },
+  photoCredits: {
+    "Rahul Mishra":
+      "https://unsplash.com/photos/a-computer-screen-with-a-bunch-of-words-on-it-Zs5X1KnHUzw",
+    Jona: "https://unsplash.com/photos/a-3d-model-of-a-structure-with-red-and-blue-balls-ft6TweD_VPU",
+    "Ales Nesetril":
+      "https://unsplash.com/photos/gray-and-black-laptop-computer-on-surface-Im7lZjxeLhg",
+    "Mohammad Rahmani":
+      "https://unsplash.com/photos/a-person-typing-on-a-laptop-in-the-dark-6tG4sQx0dh4",
+    Paul: "https://unsplash.com/photos/photo-of-high-rise-building-jooa9ewfj8A",
+    "Ryoji Iwata":
+      "https://unsplash.com/photos/man-in-black-formal-suit-jacket-and-pants-carrying-black-bag-while-walking-on-pedestrian-lane-during-daytime-wUZjnOv7t0g",
+    "Jorge Vasconez":
+      "https://unsplash.com/photos/man-crossing-pedestrian-lane-buYlndcNnjM",
+    "Sam Moghadam Khamseh":
+      "https://unsplash.com/photos/man-in-black-jacket-standing-on-blue-lighted-room-KJ241ZAOYwU",
+    "Mark Basarab":
+      "https://unsplash.com/photos/gray-concrete-road-between-trees-near-mountain-z8ct_Q3oCqM",
+    "Holden Baxter":
+      "https://unsplash.com/photos/empty-curved-road-oxQ0egaQMfU",
+    "Julien Riedel":
+      "https://unsplash.com/photos/gray-concrete-road-between-buildings-alGtgU3MQu4",
+    "Giorgio Trovato ":
+      "https://unsplash.com/photos/2-people-sitting-on-chair-near-window-during-sunset-hZeOr6qyiaw",
+    "Drew Dizzy Graham":
+      "https://unsplash.com/photos/woman-peeking-over-green-leaf-plant-taken-at-daytime-cTKGZJTMJQU",
+    "Hal Gatewood":
+      "https://unsplash.com/photos/assorted-color-abstract-painting-tZc3vjPCk-Q",
+  },
+};
---
+++ assets/scripts/theme.js
@@ -0,0 +1,9 @@
+export const dark = {
+  label: "dark",
+  symbol: "dark_mode",
+};
+
+export const light = {
+  label: "light",
+  symbol: "light_mode",
+};
